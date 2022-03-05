b2d7a05e8dd57182a77e699362002e290af4f30b
Skyler Shapiro <skyler.shapiro@gmail.com>
2022-03-05 12:54:02 -0500

    new feature a.py 22

--- a.py
+++ a.py
@@ -1,6 +1,8 @@
 some_list  = [1,2,3]
 def func(some_list):
-    some_list.append(4)
-
+    some_list.append(22)
+    def func2():
+        print(some_list)
+    func2()
 func(some_list)
 #print(some_list)
